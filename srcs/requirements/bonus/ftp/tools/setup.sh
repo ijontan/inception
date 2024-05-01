f06cf3ef307e9834c39c088e065ed6e56b573543
ijontan <85871000+ijontan@users.noreply.github.com>
2024-05-01 18:59:33 +0800

    done ftp

--- srcs/requirements/bonus/ftp/tools/setup.sh
+++ srcs/requirements/bonus/ftp/tools/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-service vsftpd start
+# service vsftpd start
 
 useradd $FTP_USER
 
@@ -10,28 +10,22 @@ echo "$FTP_USER:$FTP_PWD" | /usr/sbin/chpasswd
 echo "$FTP_USER" >> /etc/vsftpd.userlist 
 
 mkdir /home/$FTP_USER /home/$FTP_USER/ftp
+mkdir -p /var/www/html
+chown $FTP_USER:$FTP_USER /var/www/html
 
 
-chown nobody:nogroup /home/$FTP_USER/ftp
-chmod a-w /home/$FTP_USER/ftp
 
-mkdir /home/$FTP_USER/ftp/files
-chown $FTP_USER:$FTP_USER /home/$FTP_USER/ftp/files
+# chown nobody:nogroup /home/$FTP_USER/ftp
+# chmod a-w /home/$FTP_USER/ftp
+#
+# mkdir /home/$FTP_USER/ftp/files
+# chown $FTP_USER:$FTP_USER /home/$FTP_USER/ftp/files
 
-sed -i -r "s/#write_enable=YES/write_enable=YES/1"   /etc/vsftpd.conf
-sed -i -r "s/#chroot_local_user=YES/chroot_local_user=YES/1"   /etc/vsftpd.conf
-
-echo "
-local_enable=YES
-allow_writeable_chroot=YES
-pasv_enable=YES
-local_root=/home/$FTP_USER/ftp
-pasv_min_port=40000
-pasv_max_port=40005
-userlist_file=/etc/vsftpd.userlist
-" > /etc/vsftpd.conf
-
-service vsftpd stop
+rm /etc/vsftpd.conf
+cp /vsftpd.conf /etc/
 
 
-/usr/sbin/vsftpd
+# service vsftpd stop
+
+echo "done"
+/usr/sbin/vsftpd /etc/vsftpd.conf
